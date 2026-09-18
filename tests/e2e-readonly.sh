49675f1e35f80f69f1495e40b88d7b171d7ef005
krism-eu <210770240+krism-eu@users.noreply.github.com>
2026-09-18 03:34:56 +0000

    Repair RPM identity test block exactly

--- tests/e2e-readonly.sh
+++ tests/e2e-readonly.sh
@@ -110,7 +110,9 @@ test -f data/org.kriscc.KrisCC.metainfo.xml
 test ! -e data/org.kcontrolc.KControlC.metainfo.xml
 
 grep -q '^Name:[[:space:]]*krisCC$' packaging/krisCC.spec
-grep -q '^Version:[[:space:]]*0[.]5[.]0
+grep -q '^Version:[[:space:]]*0[.]5[.]0$' packaging/krisCC.spec
+grep -q '^Release:[[:space:]]*1%{?dist}$' packaging/krisCC.spec
+if grep -Eq '^Provides:[[:space:]]*(kcc|k-controlc)([[:space:]=]|$)|^Obsoletes:[[:space:]]*(kcc|k-controlc)([[:space:]<=>]|$)' packaging/krisCC.spec; then
   echo "ERROR: krisCC must not provide or obsolete experimental legacy identities" >&2
   exit 1
 fi
