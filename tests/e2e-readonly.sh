1508f82eedfdffbf8bb9eb115bfa587a25f1c8d9
krism-eu <210770240+krism-eu@users.noreply.github.com>
2026-09-18 04:11:52 +0000

    Use fixed-string 0.5.1 spec assertion

--- tests/e2e-readonly.sh
+++ tests/e2e-readonly.sh
@@ -110,7 +110,7 @@ test -f data/org.kriscc.KrisCC.metainfo.xml
 test ! -e data/org.kcontrolc.KControlC.metainfo.xml
 
 grep -q '^Name:[[:space:]]*krisCC$' packaging/krisCC.spec
-grep -q '^Version:[[:space:]]*0[.]5[.]1
+grep -Fxq 'Version:        0.5.1' packaging/krisCC.spec
 grep -q '^Release:[[:space:]]*1%{?dist}$' packaging/krisCC.spec
 if grep -Eq '^Provides:[[:space:]]*(kcc|k-controlc)([[:space:]=]|$)|^Obsoletes:[[:space:]]*(kcc|k-controlc)([[:space:]<=>]|$)' packaging/krisCC.spec; then
   echo "ERROR: krisCC must not provide or obsolete experimental legacy identities" >&2
